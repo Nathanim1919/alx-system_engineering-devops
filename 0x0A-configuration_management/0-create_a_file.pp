2d5c9d401a315f29444afd7f8280e97640e82a20
Nathanim1919 <nathanim2tadele@gmail.com>
2023-06-23 04:12:05 -0700

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+file { '/tmp/school':
+  ensure  => present,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
