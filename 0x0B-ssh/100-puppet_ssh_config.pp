605732e810c23c458fd7d2583e1cd2d89d68ec4b
Nathanim1919 <nathanim2tadele@gmail.com>
2023-11-27 23:23:42 -0800

    Client configuration file(w/Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,11 @@
 # Set up my client SSH configuration file
-file { '/root/.ssh/config':
-  ensure  => present,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0600',
-  content => '# My school server configuration
-Host 100.25.197.89
-  HostName 100.25.197.89
-  User ubuntu
-  IdentityFile ~/.ssh/school
-  PasswordAuthentication no
-'
-}
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '~/.ssh/config',
+  line   => '    PasswordAuthentication no',
+  }
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '~/.ssh/config',
+  line   => '    IdentityFile ~/.ssh/school',
+  }
