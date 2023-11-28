98b9a0ddf9b101b676358a03a3830497edb44181
Nathanim1919 <nathanim2tadele@gmail.com>
2023-11-28 01:55:20 -0800

    install a package

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+# Install flask version 2.1.0 from pip3
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  }
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  }
